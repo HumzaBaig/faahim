d79a4255da02246096db4cc7162fb68a75fd891e
HumzaBaig <humzacodemail@gmail.com>
2023-07-30 23:53:51 -0500

    create /code page component along with renaming the logo to better represent it.

--- components/loader.tsx
+++ components/loader.tsx
@@ -4,7 +4,7 @@ export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <div className="w-10 h-10 relative animate-spin">
-        <Image alt="logo" fill src="/logo.png" />
+        <Image alt="logo" fill src="/temp-logo.png" />
       </div>
       <p className="text-sm text-muted-foreground">Faahim is thinking...</p>
     </div>
