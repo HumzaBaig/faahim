57a787b0e04095c8ea86a2f01da840f1b1137695
HumzaBaig <humzacodemail@gmail.com>
2023-08-05 18:04:27 -0500

    create upgrade button with no functionality and styling in FreeCounter component.

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { MAX_FREE_COUNTS } from "@/constants";
 import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -30,6 +32,10 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
             </p>
             <Progress value={(apiLimitCount / MAX_FREE_COUNTS) * 100} />
           </div>
+          <Button className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
