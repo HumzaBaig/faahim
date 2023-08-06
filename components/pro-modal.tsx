db3e7b5ecb202c70002286d06bddf134c51b3b77
HumzaBaig <humzacodemail@gmail.com>
2023-08-06 04:39:30 -0500

    add a 'premium' variant to Badge ui component and incorporate it in ProModal component

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -63,7 +63,9 @@ export const ProModal = () => {
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
             <div className="flex items-center gap-x-2 font-bold py-1">
               Upgrade to Genius
-              <Badge className="uppercase text-sm py-1">pro</Badge>
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                pro
+              </Badge>
             </div>
           </DialogTitle>
           <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
