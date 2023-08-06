99b59c02ea665950f9b6ca2ab4daf8ab903130ac
HumzaBaig <humzacodemail@gmail.com>
2023-08-06 04:51:52 -0500

    add pro modal opening functionality to onClick in FreeCounter component Button.

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -6,6 +6,7 @@ import { MAX_FREE_COUNTS } from "@/constants";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { Zap } from "lucide-react";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -13,6 +14,7 @@ interface FreeCounterProps {
 
 export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
   const [isMounted, setIsMounted] = useState(false);
+  const proModal = useProModal();
 
   useEffect(() => {
     setIsMounted(true);
@@ -32,7 +34,11 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
             </p>
             <Progress value={(apiLimitCount / MAX_FREE_COUNTS) * 100} />
           </div>
-          <Button className="w-full" variant="premium">
+          <Button
+            onClick={proModal.onOpen}
+            className="w-full"
+            variant="premium"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
