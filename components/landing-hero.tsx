13fefd51236d229256f7a326aa536cfc3d3771ac
HumzaBaig <humzacodemail@gmail.com>
2023-08-07 18:28:04 -0500

    create button with link at the bottom of hero section

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
+import Link from "next/link";
 import TypewriterComponent from "typewriter-effect";
+import { Button } from "./ui/button";
 
 export const LandingHero = () => {
   const { isSignedIn } = useAuth();
@@ -28,6 +30,16 @@ export const LandingHero = () => {
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
         Create content using AI 10x faster.
+      </div>
+      <div>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button
+            variant="premium"
+            className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
+          >
+            Start Generating For Free
+          </Button>
+        </Link>
       </div>
     </div>
   );
